1ecd90f4691f4b432034c3ab705b0e239cfbfba6
Aciid0 <aciid0@>
2023-11-15 11:18:11 +0100

    All or Nothing challenge

---
+++ scripts/allOrNothing.config.ts
@@ -0,0 +1,62 @@
+import hre from "hardhat"
+import { setBalance } from "@nomicfoundation/hardhat-network-helpers"
+import { parseEther } from "viem"
+
+import {
+  initialiseClients,
+  buildContractClient,
+  type InitializedTutorial,
+} from "./client"
+
+import { abi } from "../artifacts/contracts/allOrNothing/AllOrNothing.sol/AllOrNothing.json"
+
+export const externalContractAddress =
+  "0x78aC353a65d0d0AF48367c0A16eEE0fbBC00aC88" as `0x${string}`
+
+export const initialize = async (): Promise<InitializedTutorial> => {
+  let contractAddress: `0x${string}`
+
+  const { walletClient, publicClient, local } = await initialiseClients()
+
+  if (local) {
+    const contract = await hre.viem.deployContract("AllOrNothing", [
+      parseEther("1"),
+      10 * 60,
+    ])
+
+    const [, , ...rest] = await hre.viem.getWalletClients()
+
+    const deposits = rest.slice(0, 5).map(async (account) => {
+      const receipt = await contract.write.bet([1, account.account.address], {
+        value: parseEther("1"),
+      })
+
+      await publicClient.waitForTransactionReceipt({
+        hash: receipt,
+      })
+    })
+
+    await Promise.all(deposits)
+
+    //  A little extra ETH than onlyPwner, to cover gas on local node
+    await setBalance(walletClient.account.address, parseEther("1.1"))
+
+    contractAddress = contract.address
+  } else {
+    contractAddress = externalContractAddress
+  }
+
+  const allOrNothinContract = buildContractClient(
+    publicClient,
+    walletClient,
+    contractAddress,
+    abi,
+  )
+
+  return {
+    walletClient,
+    publicClient,
+    local,
+    contracts: [allOrNothinContract],
+  }
+}
---
+++ test/allOrNothing.challenge.ts
@@ -0,0 +1,33 @@
+import { type PublicClient } from "viem"
+import { expect } from "chai"
+
+import type { ContractClient } from "../scripts/client"
+import { checkBalance, win } from "../scripts/checks"
+
+import { initialize } from "../scripts/allOrNothing.config"
+
+// https://onlypwner.xyz/challenges/10
+describe("Challenge: All or Nothing", () => {
+  let publicClient: PublicClient
+  let allOrNothingContract: ContractClient
+
+  before(async () => {
+    ;({
+      publicClient,
+      contracts: [allOrNothingContract],
+    } = await initialize())
+  })
+
+  it("Drain the contract of its funds", async () => {
+    //  ADD YOUR SOLUTION HERE 🤞
+  })
+
+  after(async () => {
+    const contractBalance = await checkBalance(
+      publicClient,
+      allOrNothingContract.address,
+    )
+    expect(contractBalance).to.equal(0n)
+    win()
+  })
+})
